15e6b4494c531fb0b18e29432ee85e7a1048cb3c
l1sk1sh <oliverjohnsonwork@gmail.com>
2019-05-26 13:49:53 +0300

    Timing improvement, new asserts, lots of todos

--- autoplayer/main.py
+++ autoplayer/main.py
@@ -3,6 +3,7 @@ import pyautogui as pa
 import time
 import platform
 import traceback
+
 debug = True
 
 required_processes = {
@@ -15,17 +16,20 @@ window_names = {
     "ce": "Cheat Engine 6.7"
 }
 
-# Asserts
+amount_of_matches = 12  # Maximum for modded game
+
+"""Asserts before launch"""
 if platform.system() != "Windows":
     print("Current script is designed for Windows only.")
     exit(1)
 
 local_processes = [p.name() for p in psutil.process_iter()]
-if not all(process in local_processes for process in required_processes.values()):
+if not all(pr in local_processes for pr in required_processes.values()):
     print("Seems that one of the required programs is not launched.")
     exit(1)
 
 print("Make sure that keyboard layout is English!")
+time.sleep(5)
 
 """Find coh2 icon and focus on it"""
 coh2_icon_x_y = pa.locateCenterOnScreen(
@@ -35,24 +39,57 @@ pa.click(coh2_icon_x_y)
 
 # TODO Assert that map is prepared in-game
 
+# TODO Improve logging and add in-game logging (enter chat)
 
-def en_ce():
+# TODO Add timing calculations
+
+# TODO Separate OKV config (and think about another)
+
+# TODO Add warning + assert in game about resolution
+
+# TODO Add launch arguments processing
+
+# TODO Separate map configuration (with coordinates)
+
+# TODO Think about joining play function, as first and last parts must be same
+
+# TODO Separate all coordinates and asserts corresponding to screen resolution
+
+
+def on_ce():
     """Enable CE scripts"""
     pa.keyDown("ctrl")
     pa.press("[")
     pa.keyUp("ctrl")
 
 
-def di_ce():
+def off_ce():
     """Disable CE scripts"""
     pa.keyDown("ctrl")
     pa.press("]")
     pa.keyUp("ctrl")
 
 
-def start_game():
-    """Starts match, waits for load"""
+def wait_for_element(element: str, iter_time: int, iter_amount: int):
+    count = 0
+    while count != iter_amount:
+        time.sleep(iter_time)
+        try:
+            position = pa.locateCenterOnScreen(element, confidence=0.98)
+            if position is not None:
+                return position
+            else:
+                raise TypeError
+        except TypeError:
+            count = count + 1
+            print(f"Waiting for element '{element}' for #{count} time.")
 
+    return False
+
+
+def start_match():
+    """Starts match, waits for load"""
+    # TODO Move mouse to the center of the screen
     """Find start game button and click it"""
     try:
         print("Locating 'Start match button'...")
@@ -61,42 +98,27 @@ def start_game():
             grayscale=False, confidence=0.98)
         pa.click(coh2_start_game_x_y)
     except TypeError:
-        print("Seems that button is not visible. Hitting blindly.")
+        print("Button is not visible ot match is a go. Hitting blindly...")
         pa.click(684, 704)
 
-    """Wait for game to load and try to find 'press anykey'"""
-    wait = 30  # seconds
-    match_started = False
-    count = 0
-    while count != 3:
-        time.sleep(wait)
-        try:
-            if pa.locateOnScreen('./resources/coh2_press_anykey.png',
-                                 confidence=0.98) is not None:
-                print("Starting match...")
-                pa.press("escape")
-                match_started = True
-                break
-            else:
-                raise TypeError
-        except TypeError:
-            count = count + 1
-            print(f"Waiting for game to load for #{count} time.")
-
-    if not match_started and not debug:
-        print("Could start match!")
+    """Wait for game to load and try to find 'press any key' message"""
+    if wait_for_element("./resources/coh2_press_anykey.png", 20, 4):
+        print("Starting match...")
+        pa.press("escape")
+    elif not debug:
+        print("Could not start match!")
         raise Exception
 
     time.sleep(10)  # Sleep for n seconds to skip dimming screen
 
 
 def end_match():
+    time.sleep(25)  # Usually 15s to load winning screen + 10 for boxes
     print("Closing winning screen...")
-    pa.click(608, 60)  # Close lootboxes
-    time.sleep(3)
-    pa.click(608, 60)
-    time.sleep(3)
-    pa.click(608, 60)  # Click "exit" at the winning screen
+    for c in range(4):  # Close lootboxes
+        pa.click(608, 60)  # Hit exit button
+        time.sleep(3)
+
     print("Match ended!")
 
 
@@ -104,6 +126,8 @@ def usual_match():
     """Tries to play as usual using Cheat Engine
     As for now, CE breaks victory points
     """
+
+    # TODO One squad is enough to get reward
 
     """Locate enemy base"""
     coh2_enemy_base_x_y = pa.locateCenterOnScreen(
@@ -119,12 +143,12 @@ def usual_match():
     pa.rightClick()
 
     # TODO Rewrite ordering flow to more comprehensive ans fast
-    # Folsk - 24
-    # Engineers - 28
-    # Phep - 32
+    # Folsk - 24 s
+    # Engineers - 28 s
+    # Phep - 32 s
 
     """Order units"""
-    en_ce()
+    on_ce()
     time.sleep(5)
     pa.press("v")
     time.sleep(5)
@@ -133,7 +157,7 @@ def usual_match():
     pa.press("s")
 
     """Order some more for period of time"""
-    di_ce()
+    off_ce()
     cnt = 0
     while cnt != 3:
         time.sleep(24)
@@ -142,7 +166,7 @@ def usual_match():
 
     """Order killing units and be immortal"""
     time.sleep(60)
-    en_ce()
+    on_ce()
     cnt = 0
     while cnt != 4:
         time.sleep(24)
@@ -157,57 +181,60 @@ def usual_match():
 
 
 def mod_play():
-    """Plays with Cheat Command 2 mod for 5 minutes
-    sets the winner
+    """Plays with Cheat Command 2 mod for 5 minutes and sets the winner
     """
-
-    map_central_enemy_point = (120, 679)
 
     print("Ordering units...")
     pa.press("f1")
-    pa.rightClick(map_central_enemy_point)
-    pa.press("v")
-    time.sleep(3)
-    pa.press("s")
-    time.sleep(3)
-    pa.press("v")
-    time.sleep(28)
-    pa.press("s")
-    pa.press("v")
+    pa.rightClick(120, 679)  # Point near enemy base
+    pa.press("v")  # One should be enough to pass validation
+    time.sleep(28)  # Until next is ready to be purchased
+    pa.press("v")  # Just to be sure - send another squad
+    time.sleep(20)  # Buffer time
+
+    # TODO Notify that you are free to do by Enter
+    print("Waiting for match to finish...")
+    for j in range(4):  # minutes until end
+        print(f"Waiting for {j+1} minute of match...")
+        time.sleep(60)
 
     print("Preparing for win...")
-    time.sleep(5)
+    time.sleep(3)
     pa.click(118, 12)  # button_cc_menu_x_y
     time.sleep(3)
     pa.click(96, 98)  # button_cc_menu_general_x_y
     time.sleep(3)
-    pa.click(289, 255)
+    pa.click(289, 255)  # button_cc_menu_general_win_x_y
     time.sleep(3)
     pa.click(516, 262)  # button_cc_menu_general_win_player_x_y
-
-    minutes_to_end = 4
-    print(f"Waiting for {minutes_to_end} minutes...")
-    time.sleep((60 * minutes_to_end) + 30)  # Wait for match to finish
+    time.sleep(3)
     pa.click(624, 454)  # button_cc_menu_general_win_player_ok_x_y
-    time.sleep(32)  # Wait
+    time.sleep(5)  # Count down till winning
+
     end_match()
 
 
 try:
     """Handling functions and between game throws"""
-    for i in range(4):
-        di_ce()
+    for i in range(amount_of_matches):
+        off_ce()
         print(f"Game #{i}")
-        start_game()
+        start_match()
         mod_play()
-        time.sleep(60)  # Wait for game to return to menu
-        pa.click(642, 677)  # Click summary exit button
+
+        if wait_for_element("./resources/coh2_summary_close.png", 20, 4):
+            pa.click(642, 677)  # Click summary exit button
+        elif not debug:
+            print("Could not hit summary exit button!")
+            raise Exception
+
         time.sleep(8)  # Waiting for preparation screen to load
 
     print("Execution has ended!")
 
 
-except Exception:
+except Exception as e:
     traceback.print_exc()
-    print("\nPausing the game...")
+    print(f"Seems that something is broken: '{e}'. Pausing the game...")
     pa.press("escape")
+    exit(1)
