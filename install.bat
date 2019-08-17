7aae9ec9cc404693c0393dd08cdb0065659a317f
l1sk1sh <kuzminskyoleg@yahoo.com>
2019-08-17 09:51:02 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
 @ECHO ON
 python -m pip install --upgrade pip --trusted-host pypi.org --trusted-host files.pythonhosted.org
-python -m venv .venv --python=python3.7
+python -m venv .venv
 call .\.venv\Scripts\activate.bat
 python -m pip install -r requirements.txt --trusted-host pypi.org --trusted-host files.pythonhosted.org
 EXIT 0
