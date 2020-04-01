a286302e230d0ce989d29fdd91814e6a540a10c2
ok <>
2020-04-01 16:54:55 +0300

    Removal of unused resources

--- autoplayer/config/paths.py
+++ autoplayer/config/paths.py
@@ -3,37 +3,27 @@
 
 from .config import workdir
 
-coh2_icon = workdir + "/resources/coh2_icon.png"
-steam_icon = workdir + "/resources/steam_icon.png"
+"""Company of Heroes 2 resources
+"""
+network_and_battle = workdir + "/resources/game/coh2/menu/network_and_battle.png"
+create_custom_game = workdir + "/resources/game/coh2/menu/create_custom_game.png"
 
-network_and_battle = workdir + "/resources/coh2/menu/network_and_battle.png"
-create_custom_game = workdir + "/resources/coh2/menu/create_custom_game.png"
+add_ai = workdir + "/resources/game/coh2/match/add_ai.png"
+easy_bot = workdir + "/resources/game/coh2/match/ai_easy.png"
+expert_bot = workdir + "/resources/game/coh2/match/ai_expert.png"
 
-add_ai = workdir + "/resources/coh2/match/add_ai.png"
-easy_bot = workdir + "/resources/coh2/match/ai_easy.png"
-expert_bot = workdir + "/resources/coh2/match/ai_expert.png"
+match_config_modded = workdir + "/resources/game/coh2/match/match_config_modded.png"
+match_config_real = workdir + "/resources/game/coh2/match/match_config_real.png"
 
-match_config_modded = workdir + "/resources/coh2/match/match_config_modded.png"
-match_config_real = workdir + "/resources/coh2/match/match_config_real.png"
+no_points = workdir + "/resources/game/coh2/match/no_points.png"
+start_game = workdir + "/resources/game/coh2/match/start_game.png"
+press_anykey = workdir + "/resources/game/coh2/match/press_anykey.png"
+summary_close = workdir + "/resources/game/coh2/match/summary_close.png"
 
-no_points = workdir + "/resources/coh2/match/no_points.png"
-start_game = workdir + "/resources/coh2/match/start_game.png"
-press_anykey = workdir + "/resources/coh2/match/press_anykey.png"
-summary_close = workdir + "/resources/coh2/match/summary_close.png"
+bri_symbol = workdir + "/resources/game/coh2/faction/bri_symbol_setup.png"
+ger_symbol = workdir + "/resources/game/coh2/faction/ger_symbol_setup.png"
+okw_symbol = workdir + "/resources/game/coh2/faction/okw_symbol_setup.png"
+rus_symbol = workdir + "/resources/game/coh2/faction/rus_symbol_setup.png"
+usa_symbol = workdir + "/resources/game/coh2/faction/usa_symbol_setup.png"
 
-bri_symbol = workdir + "/resources/faction/bri_symbol_setup.png"
-ger_symbol = workdir + "/resources/faction/ger_symbol_setup.png"
-okw_symbol = workdir + "/resources/faction/okw_symbol_setup.png"
-rus_symbol = workdir + "/resources/faction/rus_symbol_setup.png"
-usa_symbol = workdir + "/resources/faction/usa_symbol_setup.png"
-
-langresskaya_map = workdir + "/resources/map/langresskaya_name.png"
-
-steam_play_button = workdir + "/resources/steam/steam_play.png"
-steam_library_coh2_selected = workdir + "/resources/steam/steam_coh2_selected.png"
-steam_library_coh2_unselected = workdir + "/resources/steam/steam_coh2_unselected.png"
-steam_library_selected = workdir + "/resources/steam/steam_library.png"
-steam_library_unselected = workdir + "/resources/steam/steam_library_unselected.png"
-steam_login_title = workdir + "/resources/steam/steam_login_title.png"
-steam_news_title = workdir + "/resources/steam/steam_news_title.png"
-steam_news_title_close = workdir + "/resources/steam/steam_news_title_close.png"
+langresskaya_map = workdir + "/resources/game/coh2/map/langresskaya_name.png"
